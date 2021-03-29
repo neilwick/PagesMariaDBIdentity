ca39d09e592cd29c4268fa2b535fa17bec5e8ff6
Neil Wick <neil@wick.net>
2021-03-29 12:40:05 -0400

    custom login

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using PagesMariaDB.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace PagesMariaDB.Pages
 {
@@ -15,16 +16,23 @@ namespace PagesMariaDB.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly StoreContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger, StoreContext context)
+        private readonly UserManager<WebUser> _userManager;
+
+        public IndexModel(ILogger<IndexModel> logger, StoreContext context, UserManager<WebUser> userManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Product> Products { get; set; }
+        public WebUser WebUser {get; set;}
 
         public async Task OnGetAsync()
         {
+            if (User.Identity.IsAuthenticated) {
+                WebUser = await _userManager.GetUserAsync(User);
+            }
             var products = from product in _context.Products select product;
             Products = await products.ToListAsync();
         }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,12 +21,17 @@ namespace PagesMariaDB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(opt => {
+                opt.Conventions.AuthorizePage("/Privacy");
+            });
 
             services.AddDbContext<StoreContext>(opt => opt.UseMySql(
                 Configuration.GetConnectionString("StoreContext"),
                 ServerVersion.FromString(Configuration.GetValue<string>("mariadb-version"))
             ));
+
+            services.AddDefaultIdentity<WebUser>(opt => opt.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<StoreContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +53,7 @@ namespace PagesMariaDB
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
